7189032f79599abd5dd8903f69ce1e6df5e53bde
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-02-13 16:03:19 +0500

    feat: class-based counter component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ClassCounter from "./use-state/ClassCounter";
+
 function App() {
     return (
         <div className="App">
             <h1>Hello React Hooks</h1>
+            <ClassCounter />
         </div>
     );
 }
---
+++ src/use-state/ClassCounter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export class ClassCounter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0,
+        };
+    }
+
+    incrementCount = () => {
+        this.setState({
+            count: this.state.count + 1,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <p>Counter with class</p>
+                <button onClick={this.incrementCount}>
+                    Count: {this.state.count}
+                </button>
+            </div>
+        );
+    }
+}
+
+export default ClassCounter;
